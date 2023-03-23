647a5df91a173d3692bf162a90f8c6eeec97efcc
Χείρων <127669521+gonzapalacio@users.noreply.github.com>
2023-03-23 15:08:44 -0300

    ahora s

---
+++ ventana.py
@@ -0,0 +1,10 @@
+import tkinter#importa la librería 
+
+root  = tkinter.Tk()#crea la ventana
+
+mensaje = tkinter.Label(root, text='Santy')#crea la etiqueta
+
+mensaje.pack()#muestra la etiqueta
+
+
+root.mainloop()#mantiene la ventana para q no sé cierre
